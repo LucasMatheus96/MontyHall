fd27c1172a523ba95f264d193bc5acdbaef13548
LucasMatheus96 <lucasmatheus.martinss@gmail.com>
2025-07-04 23:57:03 -0300

    MontyHall_Finalizandoporta

--- montyhall/src/components/DoorDisplay.vue
+++ montyhall/src/components/DoorDisplay.vue
@@ -2,12 +2,13 @@
     
   <div class="door-area">
     
-    <div class="door-frame">
+    <div class="door-frame" :class="{selected: selected && !open}">
         <BoxGift></BoxGift>
     </div>
-    <div class="door">
-        <div class="number">{{ number }}</div>
-        <div class="knob"></div>
+    <div class="door" @click="selected = !selected">
+        <div class="number" :class="{selected}">{{ number }}</div>
+        <div class="knob" :class="{selected}"
+         @click="open = true"></div>
     </div>
   </div>
 </template>
@@ -35,7 +36,7 @@ export default {
 <style>
 :root {
   --door-border: 5px solid brown;
-
+  --selected-border: 5px solid yellow;
 }
 
 .door-area{
@@ -82,5 +83,32 @@ export default {
   background-color:brown;
   align-self: flex-start;
   margin-top:60px;
+}
+
+.door-frame.selected{
+  border-left:var(--selected-border);
+  border-right: var(--selected-border);
+  border-top: var(--selected-border);
+}
+
+.door .number.selected{
+  color:yellow;
+}
+
+.door > .knob.selected{
+  background-color: yellow;
+}
+
+
+.door.open {
+  background-color : #0007
+}
+
+.door.open .knob {
+  display: none;
+}
+
+.door.open .number {
+  display: none;
 }
 </style>
